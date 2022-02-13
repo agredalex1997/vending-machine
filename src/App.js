67a9556972b23633aa0958c066fb984a976ef5cb
alex agreda <agredalex1997@gmail.com>
2022-02-13 08:24:05 -0600

    refactor: minor UI changes

--- src/App.js
+++ src/App.js
@@ -30,6 +30,23 @@ function App() {
       });
   }, []);
 
+  function moveFromToBeDispatchedToDispatched(product) {
+    setToBeDispatched((prevState) =>
+      prevState.filter((iProduct) => iProduct.id !== product.id)
+    );
+    setDispatched((prevState) => [...prevState, { ...product }]);
+  }
+
+  function addSelectedProductToToBeDispatched() {
+    setToBeDispatched((prevState) => [
+      ...prevState,
+      {
+        ...selected,
+        id: selected.id + new Date().getTime(),
+      },
+    ]);
+  }
+
   return (
     <div>
       {isLoading ? (
@@ -38,57 +55,64 @@ function App() {
         <p>{error}</p>
       ) : (
         <div>
-          <h1>Selected </h1>
-          {selected && (
-            <>
-              <Product data={selected} showPreparationTime />
+          <h1>Available </h1>
+          <div style={{ display: "flex", flexWrap: "wrap", gap: "8px" }}>
+            {available.map((product) => (
               <button
+                key={product.id}
                 onClick={() => {
-                  setToBeDispatched((prevState) => [
-                    ...prevState,
-                    {
-                      ...selected,
-                      id: selected.id + prevState.length,
-                    },
-                  ]);
-                  setSelected(null);
+                  setSelected({ ...product });
+                }}
+                style={{
+                  height: "200px",
+                  width: "200px",
+                  ...(selected?.id === product.id
+                    ? { border: "5px solid green" }
+                    : {}),
                 }}
               >
-                Dispatch
+                <img src={product.thumbnail} alt={product.name} height="100" />
+                <p>{product.name}</p>
+                <p>{product.preparation_time} seconds</p>
               </button>
-            </>
-          )}
-          <h1>Available </h1>
-          {available.map((product) => (
-            <button
-              key={product.id}
-              onClick={() => {
-                setSelected({ ...product });
-              }}
-            >
-              <img src={product.thumbnail} alt={product.name} height="100" />
-              <p>{product.name}</p>
-              <p>{product.preparation_time} seconds</p>
-            </button>
-          ))}
+            ))}
+          </div>
+          <button
+            style={{ display: "block", marginTop: "8px" }}
+            disabled={!selected}
+            onClick={() => {
+              addSelectedProductToToBeDispatched();
+              setSelected(null);
+            }}
+          >
+            Dispatch
+          </button>
           <h1>To be dispatched</h1>
-          {toBeDispatched.map((product) => (
-            <Product
-              key={product.id}
-              data={product}
-              showTimeLeft
-              onReachZero={() => {
-                setToBeDispatched((prevState) =>
-                  prevState.filter((iProduct) => iProduct.id !== product.id)
-                );
-                setDispatched((prevState) => [...prevState, { ...product }]);
-              }}
-            />
-          ))}
+          <div style={{ display: "flex", flexWrap: "wrap", gap: "8px" }}>
+            {toBeDispatched.map((product) => (
+              <Product
+                key={product.id}
+                data={product}
+                showTimeLeft
+                onReachZero={() => {
+                  moveFromToBeDispatchedToDispatched(product);
+                }}
+              />
+            ))}
+          </div>
+
           <h1>Dispatched</h1>
-          {dispatched.map((product) => (
-            <Product key={product.id} data={product} />
-          ))}
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: "8px",
+            }}
+          >
+            {dispatched.map((product) => (
+              <Product key={product.id} data={product} />
+            ))}
+          </div>
         </div>
       )}
     </div>
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,20 +2,34 @@ import TimerInSeconds from "./TimerInSeconds";
 
 function Product(props) {
   return (
-    <div style={{ border: "2px solid black" }}>
-      <img src={props.data.thumbnail} alt={props.data.name} height="100" />
-      <p>{props.data.name}</p>
+    <div className="product-card">
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img
+          src={props.data.thumbnail}
+          alt={props.data.name}
+          height="100"
+          style={{
+            borderRadius: "8px",
+          }}
+        />
+      </div>
+      <p
+        style={{
+          ...(!props.showPreparationTime && !props.showTimeLeft
+            ? { textAlign: "center" }
+            : {}),
+        }}
+      >
+        {props.data.name}
+      </p>
       {props.showPreparationTime && (
         <p>{props.data.preparation_time} seconds</p>
       )}
       {props.showTimeLeft && (
-        <p>
-          <TimerInSeconds
-            initialTime={props.data.preparation_time}
-            onReachZero={props.onReachZero}
-          />{" "}
-          seconds left
-        </p>
+        <TimerInSeconds
+          initialTime={props.data.preparation_time}
+          onReachZero={props.onReachZero}
+        />
       )}
     </div>
   );
